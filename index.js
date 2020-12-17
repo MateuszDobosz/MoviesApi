e614f90e83de20cab96575667fe5c82be479559e
MateuszDobosz <mateusz.dobosz3@gmail.com>
2020-12-18 00:45:08 +0100

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const express = require('express')
+const cors = require('cors')
 const routes = require("./routes/index");
 const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.NODE_PORT
 
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/',routes);
 
